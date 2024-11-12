6840695a717c516002e536b92dac652f80e287ce
AndreeaDraghici <draghici108@gmail.com>
2024-11-12 22:22:05 +0200

    creating wishitem data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items =[
+    new WishItem('Learn Angular'),
+    new WishItem('Andreea wants coffee', true),
+    new WishItem('Find grass thhat cuts istself')
+  ]
   title = 'wishlist';
 }
---
+++ src/shared/models/wishItem.ts
@@ -0,0 +1,6 @@
+export class WishItem{
+    // isComplete by default is false
+    constructor(public wishText : string , public isComplete :boolean = false){
+
+    }
+}
