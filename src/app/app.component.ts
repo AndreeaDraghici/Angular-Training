b63c0d44d7059405b0be9f710c4be8a432884899
AndreeaDraghici <draghici108@gmail.com>
2024-11-12 22:51:14 +0200

    iterating items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// import model WishList
 import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  //created an items array that contains multiple WishItem objects
   items =[
     new WishItem('Learn Angular'),
     new WishItem('Andreea wants coffee', true),
     new WishItem('Find grass thhat cuts istself')
   ]
+
   title = 'wishlist';
 }
